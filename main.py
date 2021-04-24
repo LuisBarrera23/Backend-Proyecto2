b93603fdda891474508a06444bf9c5a242c0392a
Luis Barrera <luisbarrera5662@gmail.com>
2021-04-23 23:11:33 -0600

    subida del Backend

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask,jsonify,request
+
+from flask_cors import CORS
+
+# importacion de las clases que manejaran los objetos
+from Paciente import Paciente
+
+import json
+
+# creacion de arreglo de objetos necesarios
+
+Pacientes=[]
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/mostrarpacientes',methods=['GET'])
+def mostrarpacientes():
+    global Pacientes
+    datos=[]
+    for paciente in Pacientes:
+        objeto={
+            'nombre':paciente.getNombre(),
+            'apellido':paciente.getApellido(),
+            'fecha':paciente.getFecha(),
+            'sexo':paciente.getSexo(),
+            'usuario':paciente.getUser(),
+            'contraseña':paciente.getContraseña(),
+            'telefono':paciente.getTelefono()
+        }
+        datos.append(objeto)
+    return (jsonify(datos))
+
+    
+
+
+@app.route('/registrar',methods=['POST'])
+def guardarpaciente():
+
+    global Pacientes
+
+    nombre=request.json['nombre']
+    apellido=request.json['apellido']
+    fecha=request.json['fecha']
+    sexo=request.json['sexo']
+    usuario=request.json['usuario']
+    contraseña=request.json['contraseña']
+    telefono=request.json['contraseña']
+
+    for paciente in Pacientes:
+        if usuario==paciente.getUser():
+            return jsonify({'Mensaje':"El usuario ya existe, por favor ingrese uno diferente"})
+
+    Pacientes.append(Paciente(nombre,apellido,fecha,sexo,usuario,contraseña,telefono))
+    return jsonify({'Mensaje':"Su cuenta ha sido registrada con exito"})
+
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=3000,debug=True)
