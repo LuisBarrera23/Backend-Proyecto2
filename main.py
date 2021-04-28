51d4368354e1f35d16618aaeff94d003a12bb6ef
Luis Barrera <luisbarrera5662@gmail.com>
2021-04-28 00:57:09 -0600

    error

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def eliminarpaciente(id):
 def actualizarpaciente():
     global Pacientes
     for pacientes in Pacientes:
-        if((request.json['usuario']==pacientes.getUser()) and (request.json['id']==pacientes.getId())):
+        if((request.json['usuario']==pacientes.getUser()) and (request.json['id']==int(pacientes.getId()))):
             return jsonify({'Mensaje':'no hay problema es el mismo usuario'})
 
     return jsonify({'Mensaje':request.json['id']})
